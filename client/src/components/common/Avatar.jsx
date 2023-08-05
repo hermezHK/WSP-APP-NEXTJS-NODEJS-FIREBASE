f446941a99129a86de249aa53b8e2e80cefd1513
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-08-04 19:41:06 -0500

    feat: add profile-avatar-head(icon)

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaCamera } from "react-icons/fa";
 
-function Avatar() {
-  return <div>Avatar</div>;
+function Avatar({ type, image, setImage }) {
+  const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x:0,
+    y:0,
+  });
+
+  const showContextMenu  = (e) => {
+    e.preventDefault();
+    setContextMenuCordinates({ x: e.pageX, y: e.pageY });
+    setIsContextMenuVisible(true);
+
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        {type === "sm" && (
+          <div className="relative h-10 w-10">
+            <Image src={image} alt="avatar" className="rounded-full" fill />
+          </div>
+        )}
+        {type === "lg" && (
+          <div className="relative h-14 w-14">
+            <Image src={image} alt="avatar" className="rounded-full" fill />
+          </div>
+        )}
+        {type === "xl" && (
+          <div
+            className="relative cursor-pointer z-0"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            <div
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
+              ${hover ? "visible" : "hidden"}`}
+              onClick={e => showContextMenu(e)}
+              id="context-opener"
+            >
+              <FaCamera 
+                className="text-2xl"
+                id="context-opener"
+                onClick={e => showContextMenu(e)}/>
+              <span onClick={e => showContextMenu(e)} id="context-opener">
+                Change <br /> profile <br /> photo
+              </span>
+            </div>
+            <div className="flex items-center justify-center h-60 w-60">
+              <Image src={image} alt="avatar" className="rounded-full" fill />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Avatar;
--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,10 +1,15 @@
 import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import Input from "@/components/common/Input";
+import Avatar from "@/components/common/Avatar";
 
 function onboarding() {
 
   const [{ userInfo }] = useStateProvider();
+  const [ name, setName ] = useState(userInfo?.name || "");
+  const [ about, setAbout ] = useState("");
+  const [ image, setImage ] = useState("/default_avatar.png")
 
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
@@ -16,7 +21,11 @@ function onboarding() {
       <h2 className="text-2xl">Create profile</h2>
       <div className="flex gap-6 mt-6">
         <div className="flex flex-col items-center justify-center mt-5 gap-6">
-           { userInfo.name}
+           <Input name="Display Name" state={ name } setState={setName} label />
+           <Input name="About" state={about} setState={setAbout} label />
+        </div>
+        <div>
+          <Avatar type="xl" image={image} setImage={setImage}/>
         </div>
       </div>
     </div>
