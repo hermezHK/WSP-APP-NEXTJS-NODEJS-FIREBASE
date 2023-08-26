a437ec94d1b447f999c35ee8a95d86a1ec87a5cf
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-08-26 18:07:22 -0500

    modified credential

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_ZEGO_APP_ID : 1250977034,
-    NEXT_PUBLIC_ZEGO_SERVER_ID : "37627a3a8a66fb337d85b8089b5ac200"
+    NEXT_PUBLIC_ZEGO_APP_ID : YOUR-ID,
+    NEXT_PUBLIC_ZEGO_SERVER_ID : "YOUR-ID-SERVER"
   },
   images : {
     domains: ["localhost"],
--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // config database server user 
 const firebaseConfig = {
-    apiKey: "AIzaSyBXVxwpH1mCU97LA3jd1Np13gNAhyVQXnY",
-    authDomain: "whatsapp-clone-3f5b6.firebaseapp.com",
-    projectId: "whatsapp-clone-3f5b6",
-    storageBucket: "whatsapp-clone-3f5b6.appspot.com",
-    messagingSenderId: "301071004192",
-    appId: "1:301071004192:web:984c98550575d9ea4dc245",
-    measurementId: "G-VKK9NVW97L",
+    apiKey: "YOUR-API-KEY",
+    authDomain: "YOUR-AUTH-DOMAIN",
+    projectId: "YOUR-ID-PROJECT",
+    storageBucket: "YOUR-ID-STORAGE-BUCKET",
+    messagingSenderId: "YOUR-ID-SENDER",
+    appId: "YOUR-APP-ID",
+    measurementId: "YOUR-MEASUREMENT-ID ",
 };
 
 // Initialize Firebase
