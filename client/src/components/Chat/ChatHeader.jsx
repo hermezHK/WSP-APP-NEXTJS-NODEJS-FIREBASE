5b6cf917c26cabfc7ef7d3e0b4de36efefbe8247
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-08-26 14:29:08 -0500

    feat: exit chatheader

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
 import { MdCall } from "react-icons/md";
 import { IoVideocam } from "react-icons/io5";
@@ -6,9 +6,32 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import ContextMenu from "../common/ContextMenu";
 
 function ChatHeader() {
   const [{ currentChatUser }, dispatch] = useStateProvider();
+
+  //exit chat
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCordinates({ x: e.pageX - 50, y: e.pageY + 20 });
+    setIsContextMenuVisible(true);
+  };
+
+  const contextMenuOptions = [
+    {
+      name: "Exit",
+      callback: async () => {
+        dispatch({ type: reducerCases.SET_EXIT_CHAT });
+      },
+    },
+  ];
 
   const handleVoiceCall = () => {
     dispatch({
@@ -56,7 +79,18 @@ function ChatHeader() {
           className="text-panel-header-icon cursor-pointer text-xl"
           onClick={() => dispatch({ type: reducerCases.SET_MESSAGE_SEARCH })}
         />
-        <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+        <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" 
+        onClick={(e) => showContextMenu(e)}
+        id="context-opener"
+        />
+        {isContextMenuVisible && (
+          <ContextMenu
+            options={contextMenuOptions}
+            cordinates={contextMenuCordinates}
+            contextMenu={isContextMenuVisible}
+            setContextMenu={setIsContextMenuVisible} 
+          />
+        )}
       </div>
     </div>
   );
--- client/src/context/StateReducers.js
+++ client/src/context/StateReducers.js
@@ -111,7 +111,12 @@ const reducer = (state, action) => {
                 videoCall: undefined,
                 incomingVideoCall: undefined,
                 incomingVoiceCall: undefined,
-            };    
+            };
+        case reducerCases.SET_EXIT_CHAT:
+            return {
+                ...state,
+                currentChatUser: undefined,
+            };       
         default:
             return state;
     }
